3d3422c2f09f4a06d0b774e7c34cf88aa5c11813
BilalAhmed8959 <bilalahmed8959@gmail.com>
2024-09-08 02:51:36 -0700

    bulb on of

---
+++ app.js
@@ -0,0 +1,13 @@
+const box = document.querySelector("#box");
+const img = document.querySelector("#img");
+
+
+const abc = box.addEventListener("mouseover", (Event)=>{
+    img.src ="./realistic-light-bulb-with-electricity_23-2149129409 (1).avif"
+    Event.innerHTML = abc
+})
+const cde = box.addEventListener("mouseout", (Event)=>{
+    img.src ="./3d-realistic-off-light-bulb-icon-closeup-vector-27407044.jpg"
+    Event.innerHTML = cde
+})
+
